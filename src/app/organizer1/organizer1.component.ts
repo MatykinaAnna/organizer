17a79c2f31e225697df0ce07a9f9e478d0778f7e
MatykinaAnna <78198012+MatykinaAnna@users.noreply.github.com>
2022-04-11 17:39:16 +0300

    commit

--- src/app/organizer1/organizer1.component.ts
+++ src/app/organizer1/organizer1.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../shared/tasks.service'
+import { Task } from '../shared/tasks1.service'
 import { TaskService } from '../shared/tasks1.service';
 import { DateService } from '../shared/date.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { switchMap } from 'rxjs';
+
 
 
 @Component({
@@ -26,15 +28,45 @@ export class Organizer1Component implements OnInit {
     task_16: new FormControl(''),
   });
 
+  tasks: Task[] = []
+
   constructor(public dateService: DateService, 
              private tasksService: TaskService,
              private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    //this.dateService.date
+    this.dateService.date.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks=>{
+      this.tasks = tasks
+
+      console.log(tasks)
+    })
+  }
+
+  getTime(name: string):string{
+    return name.split('_')[1]
   }
 
   submit(){
-    console.log(this.profileForm.value);
+    // console.log(this.profileForm.value);
+
+    for (let key in this.profileForm.value){
+      if (this.profileForm.value[key] != ''){
+        const task: Task = {
+          title: this.profileForm.value[key],
+          date: this.dateService.date.value.format('DD-MM-YYYY'),
+          time: this.getTime(key)
+        }
+        console.log(task)
+        this.tasksService.create(task).subscribe(task=>{
+          console.log(task)
+          // this.tasks.push(task)
+          // this.form.reset()
+        }, err=>console.error(err))
+      }
+    }
   }
 
 }
--- src/app/shared/tasks1.service.ts
+++ src/app/shared/tasks1.service.ts
@@ -11,10 +11,33 @@ export interface Task{
     time?: string 
 }
 
+interface CreateResponse{
+    name: string
+}
+
 @Injectable({providedIn: 'root'})
 export class TaskService{
     public static url = 'https://todo-58d0f-default-rtdb.firebaseio.com/tasks '
 
     constructor(private http: HttpClient){
+    }
+
+    create(task: Task): Observable<Task>{
+        console.log(task.time)
+        return this.http.post<CreateResponse>(`${TaskService.url}/${task.date}.json/`, task)
+        .pipe(map(res => {
+            return {...task, id: res.name}
+        }))
+    }
+
+    load(date: moment.Moment){
+        return this.http
+            .get<Task[]>(`${TaskService.url}/${date.format('DD-MM-YYYY')}.json`)
+                .pipe(map(tasks => {
+                    if (!tasks) {
+                        return []
+                    }
+                    return Object.keys(tasks).map(key => ({...tasks[<any>key], id: key}))
+                }))
     }
 }    
